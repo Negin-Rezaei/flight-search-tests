8f64017794b2eb7f71667ebc80522cf50dbd207c
Negin-Rezaei <negin.rezaei@gmail.com>
2025-05-27 15:28:53 +0330

    initial commit

---
+++ pages/search/search_flight_page.js
@@ -0,0 +1,192 @@
+"use strict";
+
+import WebDriver from "selenium-webdriver";
+import { until, By } from 'selenium-webdriver';
+const SELENIUM_WAIT_TIMEOUT_VERY_SHORT = 2000;
+const SELENIUM_WAIT_TIMEOUT_SHORT = 5000;
+const SELENIUM_WAIT_TIMEOUT_MEDIUM = 20000;
+const SELENIUM_WAIT_TIMEOUT_LONG = 50000;
+
+/**
+ * Represents the Searchflight page of flytoday web application.
+ */
+export default class SearchflightPage {
+    /** 
+     * @param {!WebDriver.ThenableWebDriver} driver the selenium webdriver used
+     * by the page. 
+     */
+    constructor(driver) {
+        this._driver = driver;
+        this._selectors = {
+            // Title 
+            flyTodayPageXpath: "//div[@id='__next']//div[@id='back-to-top-anchor']//..//div[text()='پرواز داخلی']",
+            filtertitleXpath: (filtertitle) => `//label[text()='${filtertitle}']`,
+            iframeTitleId: "webpush-onsite",
+            locationSelectionXpath: (location) => `//div[@role='button']//p[text()='${location}']`,
+            // Data 
+            iconcCalendarXpath: "//span[@class='icon-calendar field_adornmentIcon__twlv1']",
+            calendarBodyXpath: "//div[@class='calendar-body_calendarWrapper__fRUc6']",
+            dateinputXpath: "//*[@id='search-bar']/div/div/section/div[2]/div/div[2]/div/div[2]/div[2]/div/div[2]/div[2]/div/div[2]/button[8]/div/span[1]",
+            // Button 
+            confirmationButtonXpath: "//button[text()='تایید']",
+            searchButtonXpath: "//button[text()='جستجو']",
+            closePopupButtonCss: "button[class*='close'], .close-button-class",
+            // Loading 
+            spinLoadingXpath: "//*[@id='__next']/div[4]/div/div[1]/div/div/div[2]",
+            // other 
+            searchSuccessfullyXpath: "//div[@class='relative mb-3 md:mb-4']",
+            filterContent: (content) => `//*[@id='search-bar']//span[contains(text(),'${content}')]`,
+            dateContent: (content) => `//*[@id="__next"]//span[contains(text(),'${content}')]`,
+        };
+    }
+
+    /**
+     * Checks if the element specified by the XPath selector is present in the DOM within the timeout.
+     * @async
+     * @returns {Promise<boolean>} Returns `true` if the element is located within the timeout, otherwise `false`.
+     */
+    async isDisplaying() {
+        try {
+            try {
+                await this._driver.wait(until.elementLocated(By.xpath(this._selectors.flyTodayPageXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+            } catch {
+                console.error();
+            }
+            return true;
+        } catch {
+            return false;
+        }
+    }
+
+    /**
+     * Clicks on the maximize icon.
+     * @async
+     * @returns {Promise<void>}  A promise that will be resolved when the method has completed.
+     */
+    async clickOnMaxIcon() {
+        await this._driver.manage().window().maximize();
+    }
+
+    /**
+     * Handles and closes the discount/notification popup if it exists inside an iframe.
+     *@async
+     *@returns {Promise<void>}  A promise that will be resolved when the method has completed.
+     */
+    async handleDiscountIframeIfExists() {
+        try {
+            await this._driver.wait(until.elementLocated(By.id(this._selectors.iframeTitleId)), SELENIUM_WAIT_TIMEOUT_SHORT);
+            const iframe = await this._driver.findElement(By.id(this._selectors.iframeTitleId));
+            await this._driver.switchTo().frame(iframe);
+            const closeButton = await this._driver.wait(until.elementLocated(By.css(this._selectors.closePopupButtonCss)), SELENIUM_WAIT_TIMEOUT_VERY_SHORT);
+            await this._driver.wait(until.elementIsVisible(closeButton), SELENIUM_WAIT_TIMEOUT_VERY_SHORT);
+            await closeButton.click();
+            await this._driver.switchTo().defaultContent();
+        } catch (e) {
+            await this._driver.switchTo().defaultContent();
+        }
+    }
+
+    /**
+     * Clicks on a filter element (source or destination) identified by the filter title.
+     * This method Handles and closes the discount iframe if it is present.
+     * @param {!string} filtertitle - The title of the filter used to build the XPath selector.
+     * @async
+     * @returns {Promise<void>}  A promise that will be resolved when the method has completed.
+     */
+    async clickOnSourceFilterAndDestinationFilter(filtertitle) {
+        await this.handleDiscountIframeIfExists();
+        const sourceFilter = await this._driver.findElement(By.xpath(this._selectors.filtertitleXpath(filtertitle)));
+        await sourceFilter.click();
+    }
+
+    /**
+     * Checks whether the expected value exists in both the filter content and the date content.
+     * This method first locates the filter and date elements using their respective XPath selectors. 
+     * Then retrieves their text content and verifies whether the expected value is included in both.
+     * @async
+     * @returns {Promise<boolean>} - `true` if the search success element is found, otherwise `false`.
+     */
+    async getDetailsFilters(content, expectValue) {
+        const filterContent = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.filterContent(content))), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        const dateContent = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.dateContent(content))), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        const ActualValueFillter = await filterContent.getText();
+        const ActualValueDate = await dateContent.getText();
+        if (ActualValueFillter.includes(expectValue) && ActualValueDate.includes(expectValue)) {
+            return true
+        }
+        else {
+            return false
+        }
+    }
+
+    /**
+     * Selects an option in a dropdown based on the given location.
+     * This method Handles and closes the discount iframe if it is present.
+     * @param {!string} location - The location value used to build the XPath selector for the dropdown option.
+     * @async
+     * @returns {Promise<void>} A promise that will be resolved when the method has completed.
+     */
+    async selectLocationInDropDown(location) {
+        await this.handleDiscountIframeIfExists();
+        const commandSet = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.locationSelectionXpath(location))), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await this._driver.wait(until.elementIsVisible(commandSet), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await commandSet.click();
+    }
+
+    /**
+     * Opens the calendar widget and selects a date.
+     * This method Handles and closes the discount iframe if it is present.
+     * @async
+     * @returns {Promise<void>} A promise that will be resolved when the method has completed.
+     */
+    async selectDateInput() {
+        await this.handleDiscountIframeIfExists();
+        const iconcCalendar = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.iconcCalendarXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await iconcCalendar.click();
+        await this._driver.wait(until.elementLocated(By.xpath(this._selectors.calendarBodyXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        const dateinput = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.dateinputXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await this._driver.wait(until.elementIsVisible(dateinput), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await dateinput.click();
+        const confirmationButton = await this._driver.findElement(By.xpath(this._selectors.confirmationButtonXpath));
+        await confirmationButton.click();
+    }
+
+    /**
+     * Clicks on the search button on the page.
+     * This method Handles and closes the discount iframe if it is present.
+     * @async
+     * @returns {Promise<void>} A promise that will be resolved when the method has completed.
+     */
+    async clickOnSearchButton() {
+        await this.handleDiscountIframeIfExists();
+        const searchButton = await this._driver.wait(until.elementLocated(By.xpath(this._selectors.searchButtonXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await searchButton.click();
+    }
+
+    /**
+     * Waits for the loading spinner to appear and then disappear.
+     * This method is used after clicking the search button to ensure that the results page has finished loading before continuing.
+     * @returns {Promise<void>} A promise that will be resolved when the method has completed.
+     */
+    async waitForLoad() {
+        await this._driver.wait(until.elementLocated(By.xpath(this._selectors.spinLoadingXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+        await this._driver.wait(until.stalenessOf(await this._driver.findElement(By.xpath(this._selectors.spinLoadingXpath))), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+    }
+
+    /**
+     * Checks if the search was completed successfully by verifying
+     * This method waits for the loading spinner to finish (via `waitForLoad`).
+     * Then checks if the search success element is present on the page.
+     * @async
+     * @returns {Promise<boolean>} - `true` if the search success element is found, otherwise `false`.
+     */
+    async isSearchSuccessful() {
+        await this.waitForLoad();
+        try {
+            await this._driver.wait(until.elementsLocated(By.xpath(this._selectors.searchSuccessfullyXpath)), SELENIUM_WAIT_TIMEOUT_MEDIUM);
+            return true;
+        } catch {
+            return false;
+        }
+    }
+};
---
+++ test/domestic_flight_search_test.js
@@ -0,0 +1,71 @@
+
+"use strict";
+
+import { Builder } from "selenium-webdriver";
+import chrome from "selenium-webdriver/chrome.js";
+import SearchflightPage from "../pages/search/search_flight_page.js";
+import assert from "assert";
+
+
+let driver;
+const MOCHA_TEST_TIMEOUT = 90000;
+/**@type {SearchflightPage} */
+let searchflightPage;
+
+suite("تست سوئیت - سایت فلای تودی", function () {
+    this.timeout(MOCHA_TEST_TIMEOUT);
+
+    suiteSetup(async function () {
+        driver = await new Builder().forBrowser("chrome").setChromeOptions(new chrome.Options()).build()
+        searchflightPage = new SearchflightPage(driver);
+        await driver.get("https://www.flytoday.ir/");
+    });
+
+    // مراحل تست:
+    // سایت فلای تودی باز می شود
+    // صفحه ماکسیمایز می‌شود
+    // اسلاید "پرواز داخلی" به صورت پیش فرض انتخاب شده نشان داده می شود
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // بر روی فیلتر "مبدا" کیلیک می شود
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // از روی لیست مقدار "تهران" جستجو و انتخاب می شود
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // بر روی فیلتر "مقصد" کیلیک می شود
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // از روی لیست مقدار "مشهد" جستجو و انتخاب می شود
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // بر روی فیلد "تاریخ" کیلیک می شود
+    // از لیت یک تاریخ انتخاب می شود
+    // بر روی دکمه "تایید" در فرم تاریخ کیلیک می شود 
+    //بررسی می شود در صوت نمایش پیام پاپ آپ این فرم بسته شود
+    // بر روی دکمه "جستجو" کیلیک می شود
+
+    test("سرچ پرواز داخلی", async function () {
+
+        const sourceFilterTitle = "مبدا";
+        const destinationFilterTitle = "مقصد";
+        const sourceLocation = "تهران";
+        const destinationLocation = "مشهد";
+        const sourceFilterContent = "تهران";
+        const destinationFilterContent = "مشهد";
+        const dateFilterContent = "تیر";
+        const expectedValueForDate="8 تیر";
+
+        assert.ok(await searchflightPage.isDisplaying());
+        await searchflightPage.clickOnMaxIcon();
+        await searchflightPage.clickOnSourceFilterAndDestinationFilter(sourceFilterTitle);
+        await searchflightPage.selectLocationInDropDown(sourceLocation);
+        assert.ok(await searchflightPage.getDetailsFilters(sourceFilterContent, sourceLocation));
+        await searchflightPage.clickOnSourceFilterAndDestinationFilter(destinationFilterTitle);
+        await searchflightPage.selectLocationInDropDown(destinationLocation);
+        assert.ok(await searchflightPage.getDetailsFilters(destinationFilterContent, destinationLocation));
+        await searchflightPage.selectDateInput();
+        assert.ok(await searchflightPage.getDetailsFilters(dateFilterContent, expectedValueForDate));
+        await searchflightPage.clickOnSearchButton();
+        await searchflightPage.isSearchSuccessful();
+    });
+    //مرورگر کروم بسته می شود.
+    suiteTeardown(async function () {
+        await driver.quit();
+    });
+});
